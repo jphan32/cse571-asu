d295b53b636b941caa25c12ecaeed1ec7c255a04
Sangjoon Han <jphan32@me.com>
2023-11-19 02:38:01 +0900

    Feat: logging and early stopping and ...

--- train_model.py
+++ train_model.py
@@ -9,6 +9,8 @@ from tqdm import tqdm
 
 import numpy as np
 import random
+import wandb
+import sys
 
 def fix_seed(random_seed):
     torch.manual_seed(random_seed)
@@ -20,15 +22,33 @@ def fix_seed(random_seed):
     random.seed(random_seed)
 
 
-def train_model(no_epochs):
+def train_model(no_epochs, test_name="test", random_seed=32):
+    fix_seed(random_seed)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     batch_size = 128
+    learning_rate = 0.001
+    early_stopping_patience = 5 
+    min_test_loss = float('inf')
+    epochs_no_improve = 0
+
     data_loaders = Data_Loaders(batch_size)
     model = Action_Conditioned_FF().to(device)
 
     loss_function = nn.BCELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    wandb.init( entity="jphan32",
+                project="CSE-571",
+                name=test_name,
+                config={
+                    "learning_rate": learning_rate,
+                    "architecture": "Action_Conditioned_FF",
+                    "dataset": "Custom Robot Sensor Data",
+                    "epochs": no_epochs,
+                    "seed": random_seed
+                })
 
     losses = []
     min_loss = model.evaluate(model, data_loaders.test_loader, loss_function)
@@ -36,6 +56,7 @@ def train_model(no_epochs):
 
     for epoch_i in tqdm(range(no_epochs)):
         model.train()
+        total_train_loss = 0.0
         for idx, sample in enumerate(data_loaders.train_loader):
             inputs = sample['input'].to(device)
             labels = sample['label'].to(device)
@@ -43,16 +64,29 @@ def train_model(no_epochs):
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = loss_function(outputs, labels)
+            total_train_loss += loss.item()
             loss.backward()
             optimizer.step()
+        average_train_loss = total_train_loss / len(data_loaders.train_loader)
 
-        test_loss = model.evaluate(model, data_loaders.test_loader, loss_function)
-        losses.append(test_loss)
+        average_test_loss = model.evaluate(model, data_loaders.test_loader, loss_function)
+
+        wandb.log({"average_train_loss": average_train_loss, "average_test_loss": average_test_loss})
+
+        if average_test_loss < min_test_loss:
+            min_test_loss = average_test_loss
+            epochs_no_improve = 0
+            torch.save(model.state_dict(), 'saved/best_model.pkl')
+        else:
+            epochs_no_improve += 1
+            if epochs_no_improve == early_stopping_patience:
+                print(f'Early Stopping : {epoch_i} epoch')
+                break
 
     torch.save(model.state_dict(), 'saved/saved_model.pkl')
+    wandb.finish()
 
 
 if __name__ == '__main__':
-    no_epochs = 100
-    fix_seed(32)
-    train_model(no_epochs)
+    no_epochs = 1000
+    train_model(no_epochs, test_name = sys.argv[1] if len(sys.argv) > 1 else "test")
